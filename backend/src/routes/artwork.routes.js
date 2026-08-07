3afa42fff6c813ef3674174ea01b9e680f95e377
FlorenciaCampos <florenciacampos460@gmail.com>
2026-08-07 14:56:26 -0300

    Finaliza autenticación JWT y protege rutas de administración

--- backend/src/routes/artwork.routes.js
+++ backend/src/routes/artwork.routes.js
@@ -1,8 +1,8 @@
 // src/routes/artwork.routes.js
-import { authenticate } from "../auth/auth.middleware.js";
 
 import { Router } from "express";
 
+import { authenticate } from "../auth/auth.middleware.js";
 import upload from "../middlewares/upload.middleware.js";
 
 import {
@@ -15,16 +15,19 @@ import {
 const router = Router();
 
 router.post(
-    "/",
-    authenticate,
-    upload.single("image"),
-    createArtwork
-  );
+  "/",
+  authenticate,
+  upload.single("image"),
+  createArtwork
+);
+
 router.get("/", getAllArtworks);
+
 router.get("/:id", getArtworkById);
 
 router.put(
   "/:id",
+  authenticate,
   upload.single("image"),
   updateArtwork
 );
